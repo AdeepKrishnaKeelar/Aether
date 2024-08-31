986f01a1911792daf13372bd96c6661e8ed3935d
AdeepKrishnaKeelar <eippeekeelar@gmail.com>
2024-08-31 23:38:58 +0530

    added the other mysql helper functions and base for the function

---
+++ src/aether-cli-tool/bash-cli/go/cmd/discover_node.go
@@ -0,0 +1,13 @@
+// The logic for the Discover Node.
+package cmd
+
+/*
+	1. Parse the flags.
+		Make sure the flags aren't empty, and make sure the flags are valid.
+	2. Confirm the validation by checking the IP by pinging the VM, and getting the details.
+	3. After getting the details, enter the details to the DB.
+*/
+
+func DiscoverNode(args []string) {
+
+}
---
+++ src/aether-cli-tool/bash-cli/go/commons/helpers/mysql_helpers.go
@@ -0,0 +1,28 @@
+// MySQL Support Functions.
+package helpers
+
+import (
+	"database/sql"
+	"log"
+
+	_ "github.com/go-sql-driver/mysql"
+)
+
+var PORT = "3306"
+
+// Function to setup DB connection.
+func Database_Connection(db_addr, db_user, db_pass, db_name string) (*sql.DB, error) {
+	// DB format --> mysql,db_user:db_pass@tcp(db_addr:PORT)/db_name
+	db, err := sql.Open("mysql", db_user+":"+db_pass+"@tcp("+db_addr+":"+PORT+")/"+db_name)
+	if err != nil {
+		log.Printf("Error in creating connection to DB -- %s", err)
+		return nil, err
+	}
+	//fmt.Println("Trying to establish connection to DB...")
+	err = db.Ping()
+	if err != nil {
+		log.Printf("Error in establishing connection to DB -- %s", err)
+		return nil, err
+	}
+	return db, nil
+}
